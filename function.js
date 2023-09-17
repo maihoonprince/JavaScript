99b77fcb71d02e8b00eec3b8f181a3b16e15a698
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-09-17 12:17:22 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,14 @@
+// non-primitive => array, object, function
+
+// how to create function anf recall it :- 
+function sayHello(Prince){
+    console.log("Hello, i am prince");
+    console.log("var recieved", Prince);
+}
+
+// function call :-
+sayHello(10);
+sayHello("hello"); 
+
+// we can even pass array as input in javaScript fuction:-
+sayHello([1,2,3,4,55]);
