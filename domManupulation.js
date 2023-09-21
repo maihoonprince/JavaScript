f8635f9d71f269a6cd01e8fc09677950bd12cd7d
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-09-21 06:22:32 +0530

    Add files via upload

---
+++ domManupulation.js
@@ -0,0 +1,22 @@
+// DOM :- Document Object Model
+
+// alert method show a alert on screen 
+alert("hey there i am a Alert");
+
+//prompt method :- this is also a alert but we can take input from user.
+const value = prompt("What is your name ?");
+console.log(value);      // prompt return a string value that can be used.
+
+// Two type of Object :-
+console.log(window);
+
+// we can chage our title by dom manupulation:-
+document.title = "ABCD";
+
+console.log(Document.body);   // This will print body code in console.
+
+document.write("This is hacked"); // Print on website.
+
+// code
+const name = prompt("What is your name ?");
+document.write("Hello" + name);
